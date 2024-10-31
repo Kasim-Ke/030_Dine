2f8e3aa5ab720a46150839af584e0516737b61aa
Fermented-coder <kuivalappa@gmail.com>
2024-10-31 20:34:36 +0200

    hero all sizes made maybe need fix later

---
+++ src/components/Hero.jsx
@@ -0,0 +1,46 @@
+import HeroDekstop from "../images/homepage/hero-bg-desktop.jpg";
+import HeroTablet from "../images/homepage/hero-bg-tablet.jpg";
+import HeroMobile from "../images/homepage/hero-bg-mobile.jpg";
+
+import Logo from "../images/logo.svg";
+
+export const Hero = () => {
+  return (
+    <div className=" relative lg:max-w-[1440px] lg:h-[820px] md:max-w-[768px] md:h-[992px] max-w-[375px] h-[748px] mx-auto text-white ">
+      <div className="absolute top-0">
+        <img className="lg:block hidden" src={HeroDekstop} alt="HeroDekstop" />
+        <img className=" md:hidden block" src={HeroMobile} alt="HeroMobile" />
+        <img
+          className="lg:hidden md:block hidden"
+          src={HeroTablet}
+          alt="HeroTablet"
+        />
+        <div className="absolute lg:top-0 bg-black lg:w-[500px] lg:h-full bottom-0 md:h-[60%]    w-full h-[70%]  z-20">
+          <img
+            className=" absolute lg:block hidden lg:left-[167px] lg:top-[65px]"
+            src={Logo}
+            alt="Logo"
+          />
+          <div className="lg:absolute  lg:left-[167px] lg:top-[258px]  lg:w-[507px] lg:h-[364px] lg:text-start md:w-[689px] md:h-[402px] text-center  items-center mx-auto w-[327px] h-[364px]  flex flex-col justify-between   z-20">
+            <img
+              className="lg:hidden block md:w-[103px] md:h-[40px] w-[82px] h-[32px]  "
+              src={Logo}
+              alt="logo"
+            />
+
+            <h1 className="lg:text-[80px] lg:tracking-[-1px] lg:leading-[80px] lg:px-0 md:text-[48px] md:tracking-[-0.6px] md:leading-[64px] text-[32px] tracking-[-0.4px] leading-[40px] font-light ">
+              Exquisite dining <span className="block"> since 1989 </span>
+            </h1>
+            <p className="lg:w-[445px] md:text-[20px]  md:leading-[30px] lg:px-0 md:px-12 ">
+              Experience our seasonal menu in beautiful country surroundings.
+              Eat the freshest produce from the comfort of our farmhouse.
+            </p>
+            <button className="w-[245px] h-[64px] lg:tracking-[2.5px] lg:leading-[16px] hover:bg-white hover:text-black duration-500 font-semibold border">
+              BOOK A TABLE
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "40px",
+
+      md: "658px",
+
+      lg: "1124px",
+
+      xl: "1580px",
+
+      "2xl": "1636px",
+    },
   },
   plugins: [],
 };
